9ac5e741a6adf353d1c6099f494ca9c3350f7320
Pedro Melchior de Oliveira <pedro.melchiordeoliveira@gmail.com>
2025-08-11 01:33:27 -0400

    Update database.py

--- database.py
+++ database.py
@@ -19,8 +19,26 @@ cursor.execute('''
         Nome TEXT NOT NULL,
         Horario TEXT NOT NULL,
         Data TEXT NOT NULL,
-        Vagas INTEGER,
+        Qnt_Ingressos INTEGER
+        )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Ingresso (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Cliente_ID INTEGER,
+        Evento_ID INTEGER NOT NULL,
+        FOREIGN KEY (Cliente_ID) REFERENCES Clientes(ID),
+        FOREIGN KEY (Evento_ID) REFERENCES Eventos(ID)
+        )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Alimentos (
         
                
+               
                )
+
+
 ''')
