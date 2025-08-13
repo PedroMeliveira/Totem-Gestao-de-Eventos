8a48d9496e12414fc27c00d23175c269f686f046
Mateus A. Muniz S. <mateuszinum@gmail.com>
2025-08-13 08:43:31 -0400

    Divisão entre admin e cliente

--- cliente.py
+++ cliente.py
@@ -3,6 +3,10 @@ import sqlite3
 import bcrypt
 
 
+conexao = sqlite3.connect('dados.db')
+cursor = conexao.cursor()
+
+
 def pagina_meus_ingressos():
     st.header("Meus Ingressos")
     st.write("---")
@@ -172,47 +176,6 @@ def pagina_central_eventos():
             st.success("Compra finalizada com sucesso!")
 
 
-def pagina_selecionar_evento_admin():
-    st.header("Selecionar Evento Admin")
-
-def pagina_estatisticas_evento():
-    st.header("Estatísticas do Evento")
-
-def pagina_configurar_area_alimentos():
-    st.header("Configurar Área de Alimentos")
-
-def pagina_configurar_central_eventos():
-    st.header("Configurar Central de Eventos")
-
-def pagina_configurar_ingresso_especifico():
-    st.header("Configurar Ingresso Específico")
-
-
-def checaLogin(email, senha):
-    conexao = sqlite3.connect('dados.db')
-    cursor = conexao.cursor()
-
-    cursor.execute("SELECT Email FROM Clientes WHERE Email = ?", (email,))
-
-    user_data = cursor.fetchone()
-
-    if user_data is not None:
-        nome, senha_bd = user_data
-        senha_bytes = senha.encode('uft-8')
-        if bcrypt.checkpw(senha_bytes, senha_bd):
-            st.session_state.nome_cliente = nome
-            st.session_state.role = "cliente"
-            pass
-        
-        else:
-            st.error("Senha inválida, tente novamente.")
-            pass
-    else:
-        st.error("Esse email não está cadastrado em nossos sistemas.")
-        pass
-
-
-
 def pagina_login():
     st.title("Bem-vindo")
     st.divider()
@@ -232,47 +195,40 @@ def pagina_login():
         )
 
     with col2:
+        def mudarCadastrar():
+            st.session_state.auth_user = "cadastrar"
+            
         col3, col4 = st.columns(2)
         with col3:
             st.markdown("###### Não possui conta?")
         with col4:
-            st.button("Cadastrar", use_container_width=True, on_click=mudarCadastrar, args=())
+            st.button("Cadastrar", use_container_width=True, on_click=mudarCadastrar)
 
 
-def mudarCadastrar():
-    st.session_state.auth_user = "cadastrar"
+def checaLogin(email, senha):
+    query = "SELECT Nome, Senha FROM Clientes WHERE Email = %s"
+    cursor.execute(query, (email,))
+
+    user_data = cursor.fetchone()
+
+    if user_data is not None:
+        nome, senha_bd = user_data
+        senha_bytes = senha.encode('uft-8')
+        if bcrypt.checkpw(senha_bytes, senha_bd):
+            st.session_state.nome_cliente = nome
+            st.session_state.role = "cliente"
+            pass
+        
+        else:
+            st.error("Senha inválida, tente novamente.")
+            pass
+    else:
+        st.error("Esse email não está cadastrado em nossos sistemas.")
+        pass
 
 
 def ir_para_login():
     st.session_state.auth_user = "login"
-
-
-def realizaCadastro(nome, cpf, email, data_nascimento, senha):
-    conexao = sqlite3.connect('dados.db')
-    cursor = conexao.cursor()
-
-    cursor.execute("SELECT Email FROM Clientes WHERE Email = ?", (email,))
-
-    resultado = cursor.fetchone() 
-
-    if resultado is not None:
-        st.error("Esse email já está cadastrado, utilize outro email ou faça login.")
-        pass
-
-    else:
-        senha_bytes = senha.encode('utf-8')
-        sal = bcrypt.gensalt()
-        senha_hash = bcrypt.hashpw(senha_bytes, sal)
-
-        conexao = sqlite3.connect('dados.db')
-        cursor = conexao.cursor()
-
-        cursor.execute("INSERT INTO Clientes (Nome, Data_Nasc, Email, Senha) VALUES (?, ?, ?, ?)", (nome, data_nascimento, email, senha_hash))
-        conexao.commit()
-
-        cliente_id = cursor.lastrowid
-        st.session_state.cliente_id = cliente_id
-        ir_para_login()
 
 
 def pagina_cadastrar():
@@ -282,11 +238,11 @@ def pagina_cadastrar():
     nome = st.text_input("Insira seu nome")
     email = st.text_input("Insira seu email")
     data_nascimento = st.date_input("Insira sua data de nascimento")
-    senha = st.text_input("Insira sua senha")
-    
+    senha = st.text_input("Insira sua senha", type="password")
+
     cpf = "1"
 
-    col1, col2 = st.columns(2)
+    col1, col2, col3= st.columns(3)
     with col1:
         st.button(
             "Cadastro",
@@ -296,18 +252,41 @@ def pagina_cadastrar():
             args=(nome, cpf, email, data_nascimento, senha)
         )
     
-    with col2:
+    with col3:
         st.button(
             "Voltar ao Login",
-            use_container_width=True,
             on_click=ir_para_login
         )
 
+
+def realizaCadastro(nome, cpf, email, data_nascimento, senha):
+    query = "SELECT Email FROM Clientes WHERE Email = %s"
+    cursor.execute(query, (email,))
+
+    resultado = cursor.fetchone() 
+
+    if resultado is not None:
+        st.error("Esse email já está cadastrado, utilize outro email ou faça login.")
+        pass
+
+    else:
+        senha_bytes = bcrypt.senha.encode('utf-8')
+        sal = bcrypt.gensalt()
+        senha_hash = bcrypt.hashpw(senha_bytes, sal)
+
+        cursor.execute("INSERT INTO Clientes (Nome, Data_Nasc, Email, Senha) VALUES (?, ?, ?, ?)" (nome, data_nascimento, email, senha_hash))
+        conexao.commit()
+
+        cliente_id = cursor.lastrowid()
+        st.session_state.cliente_id = cliente_id
+        ir_para_login()
+        
 
 if "role" not in st.session_state:
     st.session_state.role = None
 if "auth_user" not in st.session_state:
     st.session_state.auth_user = "login"
+
 
 if st.session_state.role is None:
     if st.session_state.auth_user == "login":
@@ -315,20 +294,11 @@ if st.session_state.role is None:
     elif st.session_state.auth_user == "cadastrar":
         pagina_cadastrar()
 
+
 elif st.session_state.role == "cliente":
     nav = st.navigation([
         st.Page(pagina_meus_ingressos, title="Meus Ingressos", icon="🎟️"),
         st.Page(pagina_area_alimentos, title="Área de Alimentos", icon="🍔"),
         st.Page(pagina_central_eventos, title="Central de Eventos", icon="🎉"),
     ])
-    nav.run()
-
-elif st.session_state.role == "admin":
-    nav = st.navigation([
-        st.Page(pagina_selecionar_evento_admin, title="Selecionar Evento Admin", icon="📋"),
-        st.Page(pagina_estatisticas_evento, title="Estatísticas do Evento", icon="📊"),
-        st.Page(pagina_configurar_area_alimentos, title="Configurar Área de Alimentos", icon="🍽️"),
-        st.Page(pagina_configurar_ingresso_especifico, title="Configurar Ingresso Específico", icon="🎫"),
-        st.Page(pagina_configurar_central_eventos, title="Configurar Central de Eventos", icon="🛠️"),
-    ],)
     nav.run()
